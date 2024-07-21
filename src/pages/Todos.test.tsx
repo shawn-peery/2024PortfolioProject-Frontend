87dd186ee6b613efedb6960b2efbb82edcb2cfc6
Shawn Peery <shawn.w8465@gmail.com>
2024-07-21 16:46:22 -0600

    Adding todos page

--- src/pages/Todos.test.tsx
+++ src/pages/Todos.test.tsx
@@ -1,6 +1,35 @@
-import { describe } from "vitest";
+import { BrowserRouter } from "react-router-dom";
+import { describe, expect, it } from "vitest";
+import Todos from "./Todos"
+import { render, screen } from "@testing-library/react";
 
 describe("Todos Tests", () => {
 
-  it("shows main navigation")
+  it("renders navigation", () => {
+    render(<BrowserRouter><Todos /></BrowserRouter>)
+
+    const navigation = screen.getByRole("navigation");
+    expect(navigation).toBeInTheDocument();
+
+  })
+
+  it("renders footer", () => {
+
+    render(<BrowserRouter><Todos /></BrowserRouter>)
+
+    const footer = screen.getByRole("contentinfo");
+
+    expect(footer).toBeInTheDocument();
+  });
+
+
+  it("renders todos header", () => {
+    render(<BrowserRouter><Todos /></BrowserRouter>)
+
+    const todosHeader = screen.getByRole("heading");
+
+    expect(todosHeader).toBeInTheDocument();
+    expect(todosHeader).toHaveTextContent("Todos");
+  });
+
 })
---
+++ src/pages/Todos.tsx
@@ -0,0 +1,8 @@
+import Layout from "../theme/Layout";
+
+const Todos = () => {
+  return <Layout>
+    <h1 role="heading">Todos</h1>
+  </Layout>
+}
+export default Todos;
