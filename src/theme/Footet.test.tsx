d865920f62dc89e959b9ddf5c4a45777d9e1e1bd
Shawn Peery <shawn.w8465@gmail.com>
2024-07-21 16:23:52 -0600

    Making test for footer

---
+++ src/theme/Footet.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import Footer from "./Footer"
+
+describe("Footer Test", () => {
+	it("renders footer text", () => {
+
+		render(<Footer />)
+
+		const footerMessage = screen.getByRole("contentinfo");
+
+		expect(footerMessage).toBeInTheDocument();
+
+		expect(footerMessage).toHaveTextContent("Made by Shawn Peery - shawn.w8465@gmail.com - 801-503-2828")
+
+	});
+});
--- src/theme/Layout.tsx
+++ src/theme/Layout.tsx
@@ -1,4 +1,4 @@
-import MainNav from "../components/MainNav"
+import MainNav from "./MainNav"
 
 const Layout = () => {
   return (
